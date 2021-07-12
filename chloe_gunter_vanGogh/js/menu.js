e66db08d69adf2bd996fc3baacd8fa8e6e13e9d0
Trish Ladd <4132874+TrishZwei@users.noreply.github.com>
2021-07-12 11:47:35 -0700

    new page

--- chloe_gunter_vanGogh/js/menu.js
+++ chloe_gunter_vanGogh/js/menu.js
@@ -1,6 +1,4 @@
-// $('button.hamburger').click( function(){
-// 	$(this).toggleClass('is-active');
-// })	
+	
 // when using click function on the burger menu, toggle the burger menu for the ul with class of global.
 
 $('html').removeClass('no-js').addClass("js");
@@ -8,17 +6,7 @@ $('html').removeClass('no-js').addClass("js");
 let menu = $('#main_nav'); //the menu itself
 let menuBtn =$('.hamburger'); //the hamburger button
 
-menuBtn.click( function(e){
-    e.preventDefault();
-    menuBtn.toggleClass('active');
-    menu.toggleClass('active');
-
-    if(menuBtn.hasClass('active')){
-
-        $('i.icon-menu').removeClass('icon-menu').addClass('icon-cancel-circled');
-    }
-    else{
-        $('i.icon-cancel-circled').removeClass('icon-cancel-circled').addClass('icon-menu');
-    }
-
+$('button.hamburger').click( function(){
+ $(this).toggleClass('is-active');
+ menu.toggleClass('active');
 })
