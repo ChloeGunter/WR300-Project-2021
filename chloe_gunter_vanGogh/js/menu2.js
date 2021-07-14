112de6fd6867d74c24dda60a5e74b684d323e518
Chloe Gunter <85974763+ChloeGunter@users.noreply.github.com>
2021-07-13 22:10:46 -0700

    Upload

--- chloe_gunter_vanGogh/js/menu2.js
+++
@@ -1,8 +0,0 @@
-	
-// when using click function on the burger menu, toggle the burger menu for the ul with class of global.
-
-$('html').removeClass('no-js').addClass("js");
-
-$('button.hamburger').click( function(){
- $('button.hamburger, #main_nav').toggleClass('is-active');
-})
